79bdd131eb9964504c057d188027da513f7e7e2d
Roopesh <roopeshbabubh@gmail.com>
2023-05-16 20:07:43 +0530

    Assignment-Operators-Program

---
+++ Java/AssignmentOperatorsProgram.java
@@ -0,0 +1,27 @@
+package CodeInJava;
+
+public class AssignmentOperatorsProgram {
+    public static void main(String[] args) {
+        int num = 10;
+
+        // Addition
+        num += 2; // To get num = 12
+        System.out.println("After addition: num = " + num);
+
+        // Subtraction
+        num -= 4; // To get num = 8
+        System.out.println("After subtraction: num = " + num);
+
+        // Multiplication
+        num *= 7; // To get num = 56
+        System.out.println("After multiplication: num = " + num);
+
+        // Division
+        num /= 4; // To get num = 14
+        System.out.println("After division: num = " + num);
+
+        // Modulus
+        num %= 12; // To get num = 2
+        System.out.println("After modulus: num = " + num);
+    }
+}
